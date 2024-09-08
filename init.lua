9298f35d894581b9d1e054478598a8d82d5e4653
Ohad Katz <ohad43@gmail.com>
2024-09-08 19:01:50 +0300

    Update init.lua
    - asm_lsp

--- init.lua
+++ init.lua
@@ -566,7 +566,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        asm_lsp
+        -- asm_lsp = {},
         -- gopls = {},
         -- pyright = {},
         rust_analyzer = {},
